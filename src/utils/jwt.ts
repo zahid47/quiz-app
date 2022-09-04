3cf1406769b5534f45b88497a28b0f4bc70d5c29
zahid <epiczahid@gmail.com>
2022-09-04 16:24:40 +0600

    refactor: refactor jwt

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,24 +3,23 @@ import log from "./logger";
 import config from "./config";
 
 interface payloadType extends JwtPayload {
-  role?: string;
+  role?: "admin" | "user";
+  for?: "access" | "refresh" | "verify-email" | "reset-pass";
 }
 
 export const signToken = (
-  userId: string,
-  role: string,
-  secret: string,
-  expiry: string,
+  audience: string,
+  secretOrPrivateKey: string,
+  expiresIn: string,
   payload: payloadType = {}
 ) => {
   const options = {
-    expiresIn: expiry,
+    expiresIn,
+    audience,
     issuer: "dynamic-quiz",
-    audience: userId,
   };
-  payload.role = role;
 
-  return JWT.sign(payload, secret, options);
+  return JWT.sign(payload, secretOrPrivateKey, options);
 };
 
 export const verifyToken = (token: string, secret: string) => {
@@ -49,13 +48,8 @@ export const generateAuthTokens = (userId: string, role: string) => {
     const access_expiry = config.ACCESS_TTL;
     const refresh_expiry = config.REFRESH_TTL;
 
-    const accessToken = signToken(userId, role, access_secret, access_expiry);
-    const refreshToken = signToken(
-      userId,
-      role,
-      refresh_secret,
-      refresh_expiry
-    );
+    const accessToken = signToken(userId, access_secret, access_expiry);
+    const refreshToken = signToken(userId, refresh_secret, refresh_expiry);
 
     return { accessToken, refreshToken };
 
