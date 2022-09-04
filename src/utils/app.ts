d149254cd2fc11256930831e679e312a29082bd3
zahid <epiczahid@gmail.com>
2022-09-04 18:13:43 +0600

    feat: add user and healthcheck routes

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -8,6 +8,9 @@ import errorHandler from "../middlewares/errorHandler";
 import limiter from "../middlewares/rateLimit";
 import config from "./config";
 
+import healthCheckRoute from "../modules/healthcheck/healthcheck.route";
+import userRoute from "../modules/user/user.route";
+
 const app: Express = express();
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
@@ -16,11 +19,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({ origin: config.CLIENT_URL }));
 app.use(limiter);
-app.use(morgan("dev"));
+if (process.env.NODE_ENV !== "test") {
+  app.use(morgan("dev"));
+}
 
 app.get("/", (_req: Request, res: Response) => {
   return res.sendStatus(200);
 });
+
+//Routes
+app.use("/healthcheck", healthCheckRoute);
+app.use("/user", userRoute);
 
 app.use(errorHandler);
 
