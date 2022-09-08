47142405871c02c26de305e6af77156e1d36ed19
zahid <epiczahid@gmail.com>
2022-09-08 19:22:51 +0600

    fix: patch quiz route

--- src/modules/quiz/quiz.route.ts
+++ src/modules/quiz/quiz.route.ts
@@ -23,7 +23,7 @@ router
   .get(validate(getQuizSchema), getQuizController)
   .patch(
     validate(updateQuizesSchema),
-    protect("admin"),
+    protect("user"),
     updateQuizController
   )
   .delete(
