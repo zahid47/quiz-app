74827e48f82f1241d77b81027b76d496901f6862
zahid <epiczahid@gmail.com>
2022-09-09 15:01:50 +0600

    fix: update endpoint

--- client/utils/axios.ts
+++ client/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL!,
   headers: {
     "Content-Type": "application/json",
   },
